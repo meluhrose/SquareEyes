36f3f4fd432ae98a19401fbb2c0b86d81279ca94
meluhrose <milvas04178@stud.noroff.no>
2025-08-18 13:48:53 +0200

    Add genre container to videos.html, update CSS for genre layout, and enhance script.js with genre filtering functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
+const API_URL = "https://v2.api.noroff.dev/square-eyes";
+
 document.addEventListener("DOMContentLoaded", () => {
     const dataContainer = document.getElementById("list-container");
-    const API_URL = "https://v2.api.noroff.dev/square-eyes";
 
     async function fetchData() {
         try {
@@ -24,16 +25,104 @@ document.addEventListener("DOMContentLoaded", () => {
                 const title = document.createElement("h3");
                 title.textContent = product.title;
 
+                const price = document.createElement("h3");
+                price.textContent = product.price;
+
+                const alt = document.createElement("h3");
+                alt.textContent = product.image.alt;
+
+                const href = document.createElement("a");
+                href.href = "../products/product.html"
+                href.textContent = "View Product";
+
                 dataContainer.appendChild(itemDiv);
                 itemDiv.appendChild(img);
                 itemDiv.appendChild(title);
+                itemDiv.appendChild(price);
+                itemDiv.appendChild(alt);
+                itemDiv.appendChild(href);
             });
 
         } catch (error) {
             console.error("Error fetching data:", error);
-            dataContainer.textContent = "Failed to load data. Please try again later.";
+            dataContainer.textContent = "Failed to load movies. Please try again later.";
         }
     }
 
     fetchData();
-});
+}); 
+
+var genres = [
+
+    {name: "Action"},
+    {name: "Family"},
+    {name: "Comedy"},
+    {name: "Drama"},
+];
+
+var genreContainer = document.getElementById("genre-container");
+
+for (var i = 0; i < genres.length; i++) {
+    var genreDiv = document.createElement("div");
+    genreDiv.classList.add("genre");
+
+    var genreName = document.createElement("button");
+    genreName.textContent = genres[i].name;
+    genreName.onclick = async () => {
+        await filterVideosByGenre(this.textContent)
+    }
+
+    genreDiv.appendChild(genreName);
+    genreContainer.appendChild(genreDiv);
+}
+
+
+
+async function filterVideosByGenre(genre){
+    var response = await fetch(API_URL);
+
+    var result = await response.json();
+    var data = result.data;
+
+    var dataContainer = document.getElementById("list-container");
+
+    var containerDiv = document.createElement("div");
+
+    data.forEach(product => {
+
+        if (product.genre !== genre){
+            return ;
+        }
+        
+        var itemDiv = document.createElement("div");
+        itemDiv.classList.add("item");
+
+        var img = document.createElement("img");
+        img.src = product.image.url;
+
+        var title = document.createElement("h3");
+        title.textContent = product.title;
+
+        var price = document.createElement("h3");
+        price.textContent = product.price;
+
+        var alt = document.createElement("h3");
+        alt.textContent = product.image.alt;
+
+        var href = document.createElement("a");
+        href.href = "../products/product.html"
+        href.textContent = "View Product";
+
+        itemDiv.appendChild(img);
+        itemDiv.appendChild(title);
+        itemDiv.appendChild(price);
+        itemDiv.appendChild(alt);
+        itemDiv.appendChild(href);
+
+        containerDiv.appendChild(itemDiv)
+    });
+
+    dataContainer.appendChild(containerDiv);
+
+    console.log("test");
+}
