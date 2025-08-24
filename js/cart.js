083e911c80debecfc5bcd4fa8999301b3e759a56
meluhrose <milvas04178@stud.noroff.no>
2025-08-24 20:43:54 +0200

    added id to cart div

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,5 @@
+
+
 var removeCartItemButtons = document.getElementsByClassName("remove-btn")
  console.log(removeCartItemButtons)
 for (var i = 0; i < removeCartItemButtons.length; i++) {
@@ -6,22 +8,5 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
         var buttonClicked = event.target
         buttonClicked.parentElement.remove()
               })
-
-}
-
-
-function addItemtoCart(title, price, imageSrc) {
-    var cartItem = document.createElement("div")
-    cartItem.classList.add("cart-item")
-    var cartItems = document.getElementsByClassName("cart-item")
-    var cartContents = ' <div class="cart-item">
-                <div class="cart-text">
-                    <img src="${imageSrc}">
-                    <p class="movie-title">${title}</p>
-                    <p class="price">${price}</p>
-                    <button class="cta remove-btn">Remove</button>
-                </div>'
-                cartItem.innerHTML = cartContent
-                cartItem.append
 
 }
