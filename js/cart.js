9a0f2245ad99b3176c1d0c6aabe6436ade47118f
meluhrose <milvas04178@stud.noroff.no>
2025-08-26 10:14:48 +0200

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,11 @@
-const API_URL = "https://v2.api.noroff.dev/square-eyes/";
+//const API_URL = "https://v2.api.noroff.dev/square-eyes/";
 
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 console.log(cart);
 const cartDisplayElement = document.getElementById("cart-container");
 
 
-async function updateCartDisplay() {
+function updateCartDisplay() {
     cartDisplayElement.innerHTML = " ";
 
     if (cart.length === 0) {
@@ -14,16 +14,17 @@ async function updateCartDisplay() {
     return;
 }
     //const response = await fetch(API_URL + productId);
-    const result = await response.json();
-   const item = result.data;
+    //const result = await response.json();
+    //const item = result.data;
 
     cartDisplayElement.innerHTML = `
                 <div>
-                <img src="${product.image.url}">
-                    <p class="movie-title">${product.title}</p>
-                    <p class="price">Total:$${product.price}</p>
+                <img src="assets/Square_Eyes_Cover8.jpeg">
+                    <p class="movie-title">Toy Story</p>
+                    <p class="price">Total: 99,99</p>
                     <button class="cta remove-btn">Remove</button></div>
             `;
 
         cartDisplayElement.appendChild(listItem);
-};
+});
+}
