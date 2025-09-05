472d685048fb663d9fa63e3ab5f081dfe3e29388
meluhrose <milvas04178@stud.noroff.no>
2025-09-05 10:05:39 +0200

    Added a total price div

--- js/cart.js
+++ js/cart.js
@@ -7,17 +7,21 @@ console.log(cart);
 const cartContainer = document.getElementById("cart-container");
 
 function updateCartDisplay(){
+    cartContainer.innerHTML = "";
     if (cart.length === 0) {
         cartContainer.innerHTML = "<p>Your cart is currently empty.</p><p>Start adding products to your cart!</p>";
         return;
     }
-    
+
+    let totalPrice = 0;
+    let productsFetched = 0;
 
     cart.forEach(item => {
-        var product = fetch(API_URL + item).then(Response => {
-
-            Response.json().then(Resp => {
-                var product = Resp.data;
+        fetch(API_URL + item)
+        .then(response => { response.json()
+            .then(Resp => {
+                const product = Resp.data;
+                totalPrice += product.price;
                 cartContainer.innerHTML += `
                     <div>
                         <img src="${product.image.url}" alt="${product.image.alt}">
@@ -26,6 +30,14 @@ function updateCartDisplay(){
                             <button id="remove-btn" class="cta">Remove</button>
                     </div>
                 `;
+
+                productsFetched ++;
+                if (productsFetched === cart.length) {
+
+                    const totalDiv = document.createElement("div");
+                    totalDiv.innerHTML = `<p>Total Price: $${totalPrice.toFixed(2)}</p>`;
+                    cartContainer.appendChild(totalDiv);
+                }
             });
 
         })
@@ -52,6 +64,7 @@ if (productIndex !== -1) {
     cart.splice(productIndex, 1);
     cartItemElement.remove();
     localStorage.setItem("cart", JSON.stringify(cart));
+    updateCartDisplay();
 }
 }
 }
