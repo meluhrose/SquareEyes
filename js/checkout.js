eab49273b53cf6395a71eccca44575b7629fab46
meluhrose <milvas04178@stud.noroff.no>
2025-09-05 13:49:55 +0200

    Move payment form to checkout page and update cart

--- js/checkout.js
+++ js/checkout.js
@@ -7,11 +7,6 @@ console.log(cart);
 const cartContainer = document.getElementById("cart-container");
 
 function updateCartDisplay(){
-    cartContainer.innerHTML = "";
-    if (cart.length === 0) {
-        cartContainer.innerHTML = "<p>Your cart is currently empty.</p><p>Start adding products to your cart!</p>";
-        return;
-    }
 
     let totalPrice = 0;
     let productsFetched = 0;
@@ -27,7 +22,6 @@ function updateCartDisplay(){
                         <img src="${product.image.url}" alt="${product.image.alt}">
                             <h2>${product.title}</h2>
                             <p>$${product.price}</p>
-                            <button id="remove-btn" class="cta">Remove</button>
                     </div>
                 `;
 
@@ -35,7 +29,7 @@ function updateCartDisplay(){
                 if (productsFetched === cart.length) {
 
                     const totalDiv = document.createElement("div");
-                    totalDiv.innerHTML = `<p>Total Price: $${totalPrice.toFixed(2)}</p><a href="checkout.html" class="cta">Proceed to Checkout</a>`;
+                    totalDiv.innerHTML = `<p>Total Price: $${totalPrice.toFixed(2)}</p>`;
                     cartContainer.appendChild(totalDiv);
                 }
             });
@@ -45,27 +39,3 @@ function updateCartDisplay(){
     });
 }
 updateCartDisplay();
-
-
-cartContainer.addEventListener("click", function(event) {
-    if (event.target.id === "remove-btn") {
-        const cartItemElement = event.target.closest("div");
-        if (cartItemElement) {
-            cartItemElement.remove();
-            
-            
-
-const productTitle = cartItemElement.querySelector("h2").textContent;
-
-const productIndex = cart.findIndex(itemId => {
-return true;
-});
-if (productIndex !== -1) {
-    cart.splice(productIndex, 1);
-    cartItemElement.remove();
-    localStorage.setItem("cart", JSON.stringify(cart));
-    updateCartDisplay();
-}
-}
-}
-});
