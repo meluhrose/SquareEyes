d8a2d6f3e971f8c48df2d077d69a16aacadece48
meluhrose <milvas04178@stud.noroff.no>
2025-09-12 09:51:33 +0200

    Refractor contact us form validation

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,24 +1,26 @@
-function validateForm() {
-    let name = document.forms["contact-form"]["name"].value;
-    if (name == "" || name == null) {
-      alert("Name must be filled out");
-      return false;
-    }
 
-    let email = document.forms["contact-form"]["email"].value;
-    if (email == "" || email == null) {
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("contact-form");
+  if (form) {
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let name = form["name"].value;
+      if (name == "" || name == null) {
+        alert("Name must be filled out");
+        return;
+      }
+      let email = form["email"].value;
+      if (email == "" || email == null) {
         alert("Email must be filled out");
-        return false;
-    }
-    let message = document.forms["contact-form"]["message"].value;
-    if (message == "" || message == null) {
-      alert("Message must be filled out");
-      return false;
-    }
-
-    document.getElementById("submit-button").addEventListener("onclick", function(event) {
-        event.preventDefault();
-        alert("Thank you for reaching out to us! We will get back to you shortly.");
-        document.getElementById("contact-form").reset();
+        return;
+      }
+      let message = form["message"].value;
+      if (message == "" || message == null) {
+        alert("Message must be filled out");
+        return;
+      }
+      alert("Thank you for contacting us! We will get back to you shortly.");
+      form.reset();
     });
-}
+  }
+});
